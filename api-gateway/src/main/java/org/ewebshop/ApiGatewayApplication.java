97d4a4762d7d5e9a74e0affe43c4796f27e312fd
ETomi24 <tamas.ernyei@gmail.com>
2022-10-01 01:13:15 +0200

    ApiGateway added to Project

---
+++ api-gateway/src/main/java/org/ewebshop/ApiGatewayApplication.java
@@ -0,0 +1,13 @@
+package org.ewebshop;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
+
+@EnableEurekaClient
+@SpringBootApplication
+public class ApiGatewayApplication {
+    public static void main(String[] args) {
+        SpringApplication.run(ApiGatewayApplication.class, args);
+    }
+}
