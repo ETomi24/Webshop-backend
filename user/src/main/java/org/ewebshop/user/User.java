8b6fc2876e4e83f24e7d6326f9821b5a3cf54c41
ETomi24 <tamas.ernyei@gmail.com>
2022-10-01 14:43:18 +0200

    Little refactor @Data and dockerCompose

--- user/src/main/java/org/ewebshop/user/User.java
+++ user/src/main/java/org/ewebshop/user/User.java
@@ -2,12 +2,14 @@ package org.ewebshop.user;
 
 import lombok.AllArgsConstructor;
 import lombok.Builder;
+import lombok.Data;
 import lombok.NoArgsConstructor;
 
 import javax.persistence.Entity;
 import javax.persistence.Id;
 import javax.persistence.Table;
 
+@Data
 @Builder
 @AllArgsConstructor
 @NoArgsConstructor
